f32b9493a6ce10575f0a93decc25ff6553bc24a7
Nguyễn Tiến Đức <ducntien12@gmail.com>
2020-03-22 17:09:41 +0700

    Solved Cut the Sticks

---
+++ CutTheSticks/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the cutTheSticks function below.
+func cutTheSticks(arr []int32) []int32 {
+	count := make([]int32, 1001)
+	distinctCount := 0
+	for _, i := range arr {
+		if count[i] == 0 {
+			distinctCount ++
+		}
+		count[i] ++
+	}
+	result := make([]int32, distinctCount)
+	i:=0
+	result[i] = int32(len(arr))
+	for _, c := range count {
+		if 0 != c && i < distinctCount-1 {
+			i++
+			result[i] = result[i-1] - c
+		}
+	}
+	return result
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arrTemp := strings.Split(readLine(reader), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	result := cutTheSticks(arr)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result) - 1 {
+			fmt.Fprintf(writer, "\n")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
