1236a8747f49263000d3d3bbb29f3ec5872058b7
Nguyễn Tiến Đức <ducntien12@gmail.com>
2020-03-22 23:32:30 +0700

    Solved Repeated String

---
+++ RepeatedString/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the repeatedString function below.
+func repeatedString(s string, n int64) int64 {
+	l := int64(len(s))
+	mod := n % l
+	countA := int64(0)
+	countALast := int64(0)
+	for i, c := range s {
+		if 'a' == c {
+			countA ++
+			if i < int(mod) {
+				countALast ++
+			}
+		}
+	}
+
+	return (countA * (n / l)) + countALast
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+
+	s := readLine(reader)
+
+	n, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+
+	result := repeatedString(s, n)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
